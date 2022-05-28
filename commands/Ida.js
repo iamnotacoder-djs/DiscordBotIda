b09f73cb7d238d9f2c7bd5a79760465a1cac3d49
idaspin <3via.idaspin@gmail.com>
2022-05-28 07:59:23 +0300

    ServerFeatures: Stats for Banner

--- commands/Ida.js
+++ commands/Ida.js
@@ -1,6 +1,8 @@
 'use strict';
 const 	BaseCommand = require('../structures/BaseCommand'),
-		{ MessageActionRow, MessageEmbed, MessageButton, MessageSelectMenu } = require('discord.js');
+		{ MessageActionRow, MessageEmbed, MessageButton, MessageSelectMenu } = require('discord.js'),
+        { createCanvas, loadImage, registerFont } = require('canvas'),
+        fs = require('fs');
 
 class ITshnik extends BaseCommand {
 
@@ -18,7 +20,7 @@ class ITshnik extends BaseCommand {
         options: this.options, 
         defaultPermission: true 
     };
-    componentsNames = [`itshnik_sm`, `itshnik_sm_slash`, `itshnik_sm_admin_roles`];
+    componentsNames = [`itshnik_sm`, `itshnik_sm_slash`, `itshnik_sm_admin_roles`, `itshnik_sm_sf`, `itshnik_sm_sf_banner`];
 
     constructor() {
         super();
@@ -52,6 +54,11 @@ class ITshnik extends BaseCommand {
 										label: `Админ-роли`,
 										description: `Выбор ролей, кому будут доступны админ-команды`,
 										value: `itshnik_sm_admin_roles`
+									}, 
+									{
+										label: `Server-features`,
+										description: `Welcome - сообщения, баннер сервера итд итп`,
+										value: `itshnik_sm_sf`
 									}
 								])
 						])
@@ -150,7 +157,31 @@ class ITshnik extends BaseCommand {
 							});
 						});
 					return true;
-				} 
+				} else if (interaction.values.includes(`itshnik_sm_sf`)) {
+					let serverBanner = 
+					client.db.get(`guilds.g${interaction.guild.id}.serverBanner`) ?? false;
+					interaction.reply({
+						components: [
+							new MessageActionRow()
+								.addComponents([
+									new MessageSelectMenu()
+										.setCustomId('itshnik_sm_sf')
+										.setPlaceholder('Серверные фишки')
+										.setMinValues(0)
+										.setMaxValues(1)
+										.addOptions([
+											{
+												label: `${!serverBanner? 'Включить': 'Выключить'} статус сервера в баннере`,
+												description: `Включение/выключение статуса сервера в баннере`,
+												value: `itshnik_sm_sf_banner`
+											}
+										])
+								])
+						], 
+						ephemeral: true
+					});
+					return true;
+				}
 				return false;
 			}
 			if (interaction.customId == "itshnik_sm_admin_roles") {
@@ -196,9 +227,94 @@ class ITshnik extends BaseCommand {
 					});
 				return true;
 			}
+			if (interaction.customId == "itshnik_sm_sf") {
+				if (interaction.values.includes(`itshnik_sm_sf_banner`)) {
+					let serverBanner = 
+					!client.db.get(`guilds.g${interaction.guild.id}.serverBanner`) ?? false;
+					client.db.set(`guilds.g${interaction.guild.id}.serverBanner`, serverBanner);
+
+					let guilds = client.db.get(`guilds.serverBanner`) ?? [];
+					if (serverBanner) {
+						guilds.push(interaction.guild.id);
+					} else {
+						guilds = guilds.filter(ele => {
+							return ele != interaction.guild.id;
+						});
+					}
+					client.db.set(`guilds.serverBanner`, guilds);
+					fs.unlinkSync(`./assets/guild_banners/${interaction.guild.id}.png`);
+					interaction.reply({
+						content: `Статус в баннере сервера ${serverBanner? 'включен': 'выключен'}`,
+						ephemeral: true
+					});
+					return true;
+				}
+			}
 		}
 		return false;
     }
+
+    async setupTimeouts(client) {
+		client.timeout5m.add('ida', async () => {
+			let guilds_ids = client.db.get(`guilds.serverBanner`) ?? [], 
+				guilds_ids_new = [];
+			guilds_ids = guilds_ids.filter(ele => { return typeof ele == 'string'; });
+			for(let i = 0; i < guilds_ids.length; i++) {
+				try {
+					let guild = await client.guilds.fetch(guilds_ids[i]);
+					guilds_ids_new.push(guild.id);
+
+					if (((client.db.get(`guilds.g${guild.id}.membersCount`) ?? 0) != guild.memberCount) && guild.premiumSubscriptionCount >= 7) {
+						client.db.set(`guilds.g${guild.id}.membersCount`, guild.memberCount);
+						this.drawServerBanner(guild);
+					}
+				} catch (e) {
+					Log.error(e);
+				}
+			}
+			client.db.set(`guilds.serverBanner`, guilds_ids_new);
+        });
+        return true;
+    }
+	
+	async drawServerBanner(guild) {
+		let imageBg, imageOverlay;
+		try {
+			// load overlays
+			let pathBg = guild.bannerURL({ format: 'png', size: 1024 });
+			if (fs.existsSync(`./assets/guild_banners/${guild.id}.png`)) {
+				pathBg = `./assets/guild_banners/${guild.id}.png`;
+			}
+			imageBg = await loadImage(pathBg);
+			imageOverlay = await loadImage(`./assets/guild_banner_overlay.png`);
+		} catch (e) {
+			return;
+		}
+
+		const canvas = createCanvas(960, 540);
+		const context = canvas.getContext('2d');
+		context.fillStyle = '#000000';
+		context.fillRect(0, 0, 960, 540);
+
+		context.drawImage(imageBg, 0, 0, 960, 540);
+		if (!fs.existsSync(`./assets/guild_banners/${guild.id}.png`)) fs.writeFileSync(`./assets/guild_banners/${guild.id}.png`, canvas.toBuffer('image/png'));
+		context.drawImage(imageOverlay, 0, 0, 960, 540);
+
+		// Members Count
+		let membersCount = `${guild.memberCount}`;
+		registerFont('./assets/ponteralt.ttf', { family: 'PonterAlt' });
+		context.font = '78pt PonterAlt';
+		context.textAlign = 'left';
+		context.fillStyle = '#000';
+		context.fillText(membersCount, 236, 470);
+		context.fillStyle = '#fff';
+		context.fillText(membersCount, 234, 468);
+		guild.setBanner(canvas.toBuffer('image/png'))
+			.then((updated) => {
+				Log.send(`Updated the guild banner for ${guild.name} (${guild.id})`);
+			})
+			.catch(Log.error);
+	}
 }
 
 module.exports = ITshnik
