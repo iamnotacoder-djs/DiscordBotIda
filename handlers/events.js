3b26e3a1f6a80ab88e494727893f2a68b71f4053
Anton Pitimirov <3via.idaspin@gmail.com>
2022-08-30 16:29:31 +0300

    We need more promises

--- handlers/events.js
+++ handlers/events.js
@@ -1,14 +1,25 @@
-const fs = require('fs');
+const { lstat, readdir } = require('fs/promises'),
+      { join } = require('path');
 
 module.exports.init = async (client) => {
     Log.send(`[HANDLER/EVENTS] Хандлер событий запущен.`);
-    fs.readdirSync(`./events`).filter(s => !s.startsWith('_') && s.endsWith('.js')).forEach(file => {
-        const event = require(`../events/${file}`);
+    walk(client, './events/');
+}
+
+async function walk(client, dir) {
+    if (Array.isArray(dir)) return;
+    if ( !(await lstat(dir)).isDirectory() ) {
+        const event = require(`../${dir}`);
         if (event.once) {
             client.once(event.name, (...args) => event.execute(client, ...args));
         } else {
             client.on(event.name, (...args) => event.execute(client, ...args));
         }
         Log.send(`[HANDLER/EVENTS] Слушатель "${event.name}" загружен.`);
-    });
+        return;
+    }
+    for(let file of (await readdir(dir))) {
+        await walk(client, join(dir, file));
+    }
+    return;
 }
