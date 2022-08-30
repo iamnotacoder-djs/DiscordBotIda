5170ca25c7fb3774e07f18e00488f58a19278a5c
Anton Pitimirov <3via.idaspin@gmail.com>
2022-08-30 16:29:08 +0300

    We need more promises

--- handlers/commands.js
+++ handlers/commands.js
@@ -1,25 +1,33 @@
-const   fs = require('fs'),
+const   { lstat, readdir } = require('fs/promises'),
+        { join } = require('path'),
         BaseCommand = require('../structures/BaseCommand');
 
 module.exports.init = async (client) => {
-    Log.send(`[HANDLER/COMMANDS] Хандлер Slash-комманд запущен.`);
-    let slashes = [];
-    fs.readdirSync(`./commands`).filter(s => !s.startsWith('_') && s.endsWith('.js')).forEach(file => {
-		const cmdClass = require(`../commands/${file}`);
-		const cmd = new cmdClass();
+    Log.send(`[HANDLER/EVENTS] Хандлер Slash-комманд запущен.`);
+    const slashes = await walk(client, './commands/').catch(console.error);
+    client.application.commands.set(slashes)
+        .then(() => {
+            Log.send(`[HANDLER/COMMANDS] Установлено ${slashes.length} глобальных slash-комманд.`);
+        })
+        .catch((e) => {
+            Log.error(`[HANDLER/COMMANDS] Ошибка установки глобальных slash-комманд: ${e}`);
+        });
+    Log.send(`[HANDLER/COMMANDS] Загружено ${client.commands.size} комманд.`);
+}
+
+async function walk(client, dir, slashes = []) {
+    if (Array.isArray(dir)) return slashes;
+    if ( !(await lstat(dir)).isDirectory() ) {
+		const cmd = new require(`../${dir}`);
 		if (cmd instanceof BaseCommand) {
 			client.commands.set(cmd.name, cmd);
-			cmd.setupTimeouts(client);
 			if (cmd.type.includes(Config.CommandType.SLASH_APPLICATION)) 
 				slashes.push(cmd.slash);
 		}
-    });
-    client.application.commands.set(slashes)
-        .then(() => {
-            Log.send(`[HANDLER/COMMANDS] Установлено ${slashes.length} дефолтных slash-комманд.`);
-        })
-        .catch((err) => {
-            Log.send(`[HANDLER/COMMANDS] Ошибка установки дефолтных slash-комманд: ${err}`);
-        });
-    Log.send(`[HANDLER/COMMANDS] Загружено ${client.commands.size} комманд.`);
+        return slashes;
+    }
+    for(let file of (await readdir(dir))) {
+        await walk(client, join(dir, file), slashes);
+    }
+    return slashes;
 }
